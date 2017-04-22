048a98250c71688f1ed1131782608950a0516e50
alfredodev <fredyfl@gmail.com>
2017-04-22 15:58:56 +0200

    spike navigation

---
+++ tomatina-front/src/components/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Dashboard
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'dashboard',
+    methods:{
+      
+    },
+    data() {
+      return {      
+      };
+    },
+  };
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ tomatina-front/src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <label>Login</label>
+      <input type="text" v-model="login" />
+    </div>
+
+    <div>
+      <label>Passowrd</label>
+      <input type="password" v-model="password"/>
+    </div>
+    <div>
+      <button v-on:click="doLogin"> login </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    methods:{
+      doLogin: function(){
+        this.$router.push('/dashboard')
+      }
+    },
+    data() {
+      return {
+        login: '',
+        password: ''
+      };
+    },
+  };
+
+</script>
+
+<style scoped>
+
+</style>
